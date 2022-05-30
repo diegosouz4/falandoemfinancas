2f3a8cd9b1095eba4f9984ac0fd5808249eb2fbd
Diego Desktop <disouzauva@gmail.com>
2022-05-30 09:07:00 -0300

    Adicionando página de cursos

--- src/js/modulos/menu.js
+++ src/js/modulos/menu.js
@@ -3,6 +3,7 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   initMenu();
   initBuscaForm();
+  initSubMenus();
 }
 
 function initMenu() {
@@ -61,4 +62,34 @@ function initBuscaForm() {
   }
 
   btnBusca.addEventListener("click", animaBarraBusca);
+}
+
+function initSubMenus() {
+  const subMenus = document.querySelectorAll(".menu-item-has-children");
+  const larguraTela = window.innerWidth;
+
+  console.log(larguraTela);
+  if (!subMenus || larguraTela > 1250) return;
+
+  subMenus.forEach((item) => {
+    const link = item.querySelector("a");
+    const btnOpenMenu = document.createElement("span");
+    link.classList.add("menu__link__submenu");
+    btnOpenMenu.classList.add("menu__btn__submenu");
+    btnOpenMenu.ariaLabel = "abrir submenu";
+    link.appendChild(btnOpenMenu);
+  });
+
+  const subMenuBtns = document.querySelectorAll(".menu__btn__submenu");
+
+  subMenuBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      event.preventDefault();
+      const alvo = event.target;
+      const pai = alvo.parentNode.parentNode;
+      const submenu = pai.querySelector(".sub-menu");
+      alvo.classList.toggle("ativo");
+      submenu.classList.toggle("ativo");
+    });
+  });
 }
